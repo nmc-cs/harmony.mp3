c1a281bc3c79a0cfd39221a246f10743cf5eef7e
nmc-cs <nitinchava10@gmail.com>
2025-05-10 14:07:39 -0500

    add playlist creation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,34 +1,59 @@
-﻿using System;
-using System.Globalization;
-using System.Windows.Data;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Harmony.Models;
+using Harmony.ViewModels;
 
-namespace Harmony.Utils
+namespace Harmony
 {
-    /// <summary>
-    /// Converts RepeatMode enum to appropriate symbol
-    /// </summary>
-    public class RepeatModeToSymbolConverter : IValueConverter
+    public partial class MainWindow : Window
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        private MainViewModel _viewModel;
+
+        public MainWindow()
         {
-            if (value == null)
-                return "🔁";
+            InitializeComponent();
 
-            RepeatMode mode = (RepeatMode)value;
-
-            return mode switch
-            {
-                RepeatMode.None => "↩️",   // No repeat
-                RepeatMode.One => "🔂",     // Repeat one
-                RepeatMode.All => "🔁",     // Repeat all
-                _ => "🔁"
-            };
+            // Initialize the ViewModel and set it as DataContext
+            _viewModel = new MainViewModel();
+            DataContext = _viewModel;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        private void TimelineSlider_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            throw new NotImplementedException();
+            // Get the slider control
+            Slider slider = sender as Slider;
+            if (slider != null)
+            {
+                // Get the position of the mouse click relative to the slider
+                Point mousePosition = e.GetPosition(slider);
+
+                // Calculate the proportion of the width
+                double proportion = mousePosition.X / slider.ActualWidth;
+
+                // Calculate the value based on the slider range
+                double sliderValue = proportion * slider.Maximum;
+
+                // Set the slider value and seek to that position
+                slider.Value = sliderValue;
+                _viewModel.SeekCommand.Execute(sliderValue);
+            }
+        }
+
+        private void PlaylistListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Update the SelectedTracks collection in the ViewModel
+            var listBox = sender as ListBox;
+            if (listBox != null)
+            {
+                var selectedItems = new ObservableCollection<AudioFile>();
+                foreach (AudioFile item in listBox.SelectedItems)
+                {
+                    selectedItems.Add(item);
+                }
+                _viewModel.SelectedTracks = selectedItems;
+            }
         }
     }
 }
